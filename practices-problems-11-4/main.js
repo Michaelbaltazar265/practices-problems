40881be133e6e885d2282362cddc7ff5beb854e9
Michaelbaltaza265 <michaelbaltazar265@gmail.com>
2020-11-05 22:59:05 +0000

    pp-11-4-2

--- practices-problems-11-4/main.js
+++ practices-problems-11-4/main.js
@@ -32,7 +32,6 @@ function circle_or_square(rad, area){
     return half
 } 
 
-// console.log ( circle_or_square(16, 625))
 ////////////////////////////////////////////////////////
 
 // Modifying the Last Character
@@ -45,8 +44,80 @@ function circle_or_square(rad, area){
 function modifyLast(str, n) {
     var lastChar = str.charAt(str.length -1);
     var repeat = lastChar.repeat(n -1)
-    console.log(repeat)
     return str + repeat;	
 } 
 
 modifyLast("Hello", 3)
+
+///////////////////////////////////////////////////////// 
+
+// Coding Website Score Calculator 
+
+// Imagine you run a website that presents users with different coding
+// challenges in levels Easy, Medium, and Hard, where users get points 
+// for completing challenges. An Easy challenge is worth 5 points,
+// a Medium challenge is worth 10 points, and a Hard challenge is worth 20 points.
+
+// Create a function that takes in the number of each challenge 
+// level a user has played and calculates the user's total 
+// number of points. Keep in mind that a user cannot 
+// complete negative challenges, so the function should 
+// return the string "invalid" if any of the passed parameters are negative.
+
+// Example 
+
+// scoreCalculator(1, 2, 3) ➞ 85
+
+// scoreCalculator(1, 0, 10) ➞ 205
+
+// scoreCalculator(5, 2, -6) ➞ "invalid" 
+
+function scoreCalculator(easy, med, hard) {
+    var easy1 = easy * 5; 
+    var med1 = med * 10; 
+    var hard1 = hard * 20; 
+    var add = easy1 + med1 + hard1; 
+    var neg = "invalid"; 
+    if ( hard < 0 || med < 0 || easy < 0 ){ 
+        return neg; 
+    }else { 
+        return add
+    }
+	return add || neg;
+} 
+
+
+
+
+///////////////////////////////////////////////////
+
+// Calculating Damage 
+
+// Create a function that takes damage and speed (attacks per second) 
+// and returns the amount of damage after a given time. 
+
+// damage(40, 5, "second")  == to get that 40 * 5 = 200
+
+// damage(100, 1, "minute") == to get that (100 * 1) * 60 = 6000 
+
+// damage(2, 100, "hour") == to get that (2 * 100) * 3600 = 720000 
+
+function damage(damage, speed, time) { 
+    var answer; 
+    if (damage < 0 || speed < 0 || time < 0 ){ 
+        answer = "invalid";
+        return answer
+    } else if (time === "second"){ 
+        answer = damage * speed;
+        return answer;
+    } else if (time === "minute"){ 
+        answer = (damage * speed) * 60;
+        return answer;
+    } else if ( time === "hour"){ 
+        answer = (damage * speed) * 3600;
+        return answer;
+    }
+return answer
+} 
+
+damage(4, 9 , "hour")
