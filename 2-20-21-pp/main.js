7ccc78538e237bfd25630fb8c573eab76f009c2b
Michael <michaelbaltazar265@gmail.com>
2021-02-02 23:11:15 +0000

    practice problem

---
+++ 2-20-21-pp/main.js
@@ -0,0 +1,15 @@
+
+function squaed(b) {
+	return b * b
+}
+
+// console.log(squaed(10))
+
+
+
+const is21 = (num) => { 
+	
+	if (num > 21 ){ 
+		return "is over 21"
+	} else return "not old enough"
+} 
