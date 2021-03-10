7b58f4cbac38f587dee2d58a2d48cf2bf4909902
Michael <michaelbaltazar265@gmail.com>
2021-03-10 05:17:57 +0000

    2-20-21-pp

--- 2-20-21-pp/main.js
+++ 2-20-21-pp/main.js
@@ -23,8 +23,31 @@ const repeatString = function(string, num) {
 
 	repeatString('hey', 3) 
 
-	console.log(repeatString('hey', 3))
-	console.log(repeatString('Mike', 3))
-	console.log(repeatString('yes', 8))
-	console.log(repeatString('lilly', 15))
+// 	console.log(repeatString('hey', 3))
+// 	console.log(repeatString('Mike', 3))
+// 	console.log(repeatString('yes', 8))
+// 	console.log(repeatString('lilly', 15)) 
 
+// 	console.log("michael" || "wrld")
+// console.log("foo" && "bar")
+
+for (var i = 0; i <= 20; i++) {
+	//console.log(i)
+//	setTimeout(() => console.log(i), 1000)
+  }
+
+
+  for(var i = 1; i < 6; i++) {
+	setTimeout(function() {
+	   console.log(i);
+	},1000);
+  }
+  console.log('The loop is done!');
+
+
+  for (var i=1; i < 101; i++){
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+}
