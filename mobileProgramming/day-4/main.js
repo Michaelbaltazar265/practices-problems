ccf5582e5f4a327cf3d3bf31d6858295b8be8da0
Michael <michaelbaltazar265@gmail.com>
2021-04-07 20:14:27 +0000

    mobileProgrammin/day-5

--- mobileProgramming/day-4/main.js
+++ mobileProgramming/day-4/main.js
@@ -271,40 +271,40 @@
 /// forEach method - changes the original Array 
 
 
-const array1 = ['a', 'b', 'c'];
+// const array1 = ['a', 'b', 'c'];
 
-// array1.forEach(element => console.log(element));
+// // array1.forEach(element => console.log(element));
 
-const array2 = [1, 5, 7, 9 ,10];
+// const array2 = [1, 5, 7, 9 ,10];
 
-// array2.forEach(e => console.log(e));
+// // array2.forEach(e => console.log(e));
 
-const emp_det=[
-            {
-                empid:'E001',
-                name:'James',
-                salary:4500
-            },
-            {
-                empid:'E002',
-                name:'John',
-                salary:4700
-            },
-            {
-                empid:'E003',
-                name:'Alex',
-                salary:4900
-            },
-            {
-                empid:'E004',
-                name:'Peter',
-                salary:5100
-            },
-        ];
+// const emp_det=[
+//             {
+//                 empid:'E001',
+//                 name:'James',
+//                 salary:4500
+//             },
+//             {
+//                 empid:'E002',
+//                 name:'John',
+//                 salary:4700
+//             },
+//             {
+//                 empid:'E003',
+//                 name:'Alex',
+//                 salary:4900
+//             },
+//             {
+//                 empid:'E004',
+//                 name:'Peter',
+//                 salary:5100
+//             },
+//         ];
 
-        emp_det.forEach(function(val){
-                    //console.log(`Name=${val.name}, Salary=${val.salary}`);
-                });
+//         emp_det.forEach(function(val){
+//                     //console.log(`Name=${val.name}, Salary=${val.salary}`);
+//                 });
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -314,3 +314,268 @@ const emp_det=[
     // emp_det.map(function(val){
     //     console.log(`Emp Id=${val.empid}, Salary=${val.salary * 2}`);
     // })
+
+    // function display(){ 
+    //     let x = 0;
+    //     while (x <= 10){ 
+    //         x++
+            
+    //         if (x < 5)
+    //         // console.log(x);
+    //         continue;
+    //         console.log("after the continue",x);
+    //     }
+        
+    // }
+
+    // display() 
+
+//     function display()
+// {
+//     let x=2,y=9,z=4,p=6;
+//     p-=(x>y)?(y>x)?(x>p)?(x-y):(y-p):(z-x):(p-x);
+//     console.log(p);
+// }
+// display(); 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Other Higher order functions
+//setTimeout
+
+// const emp_det=[
+//     {empid:'E001',name:'John',dept:'IT',sal:4000},
+//     {empid:'E002',name:'Sam',dept:'SALES',sal:4500},
+//     {empid:'E003',name:'Mac',dept:'MKTG',sal:5200},
+//     {empid:'E004',name:'James',dept:'ADMIN',sal:5800}
+// ];
+
+// function demo()
+// {
+//     setTimeout(function(){
+//         emp_det.forEach(function(val){
+//             console.log(`Emp Id=${val.empid},Salary=${val.sal}`)
+//         })
+//     },5000)
+// }
+// demo();
+
+// const emp_det=[
+//     {empid:'E001',name:'John',dept:'IT',sal:4000},
+//     {empid:'E002',name:'Sam',dept:'SALES',sal:4500},
+//     {empid:'E003',name:'Mac',dept:'MKTG',sal:5200},
+//     {empid:'E004',name:'James',dept:'ADMIN',sal:5800}
+// ];
+
+// //  function demo()
+// {
+//     setTimeout(function(){
+//         let values='';
+//         emp_det.forEach(function(val){
+//             values+=`EmpId=${val.empid}, Name=${val.name}, Dept=${val.dept}, Salary=${val.sal}`
+//             values+="<br>";
+//         })
+//         document.getElementById('p1').innerHTML+=values;
+//     },5000)
+// }
+
+// const emp_det=[
+//     {empid:'E001',name:'John',dept:'IT',sal:4000},
+//     {empid:'E002',name:'Sam',dept:'SALES',sal:4500},
+//     {empid:'E003',name:'Mac',dept:'MKTG',sal:5200},
+//     {empid:'E004',name:'James',dept:'ADMIN',sal:5800}
+// ];
+
+// function demo()
+// {
+//     setTimeout(()=>{
+//         let values='';
+//         emp_det.forEach((val)=>{
+//             values+=`EmpId=${val.empid}, Name=${val.name}, Dept=${val.dept}, Salary=${val.sal}`
+//             values+="<br>";
+//         })
+//         document.getElementById('p1').innerHTML+=values;
+//     },5000)
+// }
+
+// function add_det(det)
+// {
+//     setTimeout(()=>
+//         emp_det.push(det),3000
+//     )
+// }
+
+// add_det({empid:'E005',name:'Peter',dept:'IT',sal:6200})
+
+// const emp_det=[
+//     {empid:'E001',name:'John',dept:'IT',sal:4000},
+//     {empid:'E002',name:'Sam',dept:'SALES',sal:4500},
+//     {empid:'E003',name:'Mac',dept:'MKTG',sal:5200},
+//     {empid:'E004',name:'James',dept:'ADMIN',sal:5800}
+// ];
+
+// function demo()
+// {
+//     setTimeout(()=>{
+//         let values='';
+//         emp_det.forEach((val)=>{
+//             values+=`EmpId=${val.empid}, Name=${val.name}, Dept=${val.dept}, Salary=${val.sal}`
+//             values+="<br>";
+//         })
+//         document.getElementById('p1').innerHTML+=values;
+//     },2000)
+// }
+
+// function add_det(det)
+// {
+//     setTimeout(()=>
+//         emp_det.push(det),8000
+//     )
+// }
+
+// add_det({empid:'E005',name:'Peter',dept:'IT',sal:6200})
+
+//Callback
+// const emp_det=[
+//     {empid:'E001',name:'John',dept:'IT',sal:4000},
+//     {empid:'E002',name:'Sam',dept:'SALES',sal:4500},
+//     {empid:'E003',name:'Mac',dept:'MKTG',sal:5200},
+//     {empid:'E004',name:'James',dept:'ADMIN',sal:5800}
+// ];
+
+// function demo()
+// {
+//     setTimeout(()=>{
+//         let values='';
+//         emp_det.forEach((val)=>{
+//             values+=`EmpId=${val.empid}, Name=${val.name}, Dept=${val.dept}, Salary=${val.sal}`
+//             values+="<br>";
+//         })
+//         document.getElementById('p1').innerHTML+=values;
+//     },2000)
+// }
+
+// function add_det(det,callback)
+// {
+//     setTimeout(()=>{
+//         emp_det.push(det);
+//         callback();
+//     },3000)
+// }
+
+// add_det({empid:'E005',name:'Peter',dept:'IT',sal:6200},demo);
+
+//Promise
+const emp_det=[
+    {empid:'E001',name:'John',dept:'IT',sal:4000},
+    {empid:'E002',name:'James',dept:'SALES',sal:4200},
+    {empid:'E003',name:'Sam',dept:'ADMIN',sal:4400},
+    {empid:'E004',name:'Mac',dept:'MKTG',sal:4800}
+];
+
+function demo()
+{
+    setTimeout(()=>{
+        let values='';
+        emp_det.forEach((val)=>{
+            values+=`EmpId=${val.empid},Name=${val.name}, Dept=${val.dept}, Salary=${val.sal}<br>`
+        })
+        document.getElementById('p1').innerHTML+=values;
+    },2000)    
+}
+
+
+function add_det(det)
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            emp_det.push(det);
+            const error=false;
+            if(!error)
+            {
+                resolve();
+            }           
+            else
+            {
+                reject('Erroneous situation');
+            }
+        },3000)       
+    })
+}
+
+add_det({empid:'E005',name:'Peter',dept:'IT',sal:5200})
+.then(demo)
+.catch(err=>console.log(err))
+
+//Miscellaneous
+
+// function display()
+// {
+//     let x=-5,y=-4,p=-2;
+//     console.log(x++,++x,y++,++x,++y,p++,--p,++p,--x);
+// }
+// display();
+
+//-5,-3,-4,-2,-2,-2,-2,-1,-3
+
+// x=-2
+// y=-2
+// p=-1
+
+// function display()
+// {
+//     let x=0;
+//     while(x<=10)
+//     {
+//     console.log(x)
+//     if(x<5)
+//         continue;
+//     }
+//     x++;
+// }
+// display();
+
+sum=0;
+for(x=1;x<=5;x++)
+{
+    if(x<4)
+    {
+        continue;
+    }
+    sum=sum+x;
+}
+console.log(sum);
+
+// function display()
+// {
+//     let x=0;
+//     while(x<=10)
+//     {
+//     x++;
+//     if(x<5)
+    
+//         continue;
+//      console.log(x)
+//     }
+// }
+// display();
+
+
+
+// function display()
+// {
+//     let x=-2,y=-9,z=-4,p=-6;
+//     p-=(x>y)?(y>x)?(x>p)?(x-y):(y-p):(z-x):(p-x);
+//     console.log(p);
+// }
+// display();
+
+
+function display() { 
+    let x = 2, y = 10, z = 4;
+    let p = (z > x) ? (y < x) ? (y > z) : (x + z) : (y + z);
+    console.log(`P is ${p}`);
+}
+////// 6
+display()
