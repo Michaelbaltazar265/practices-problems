6568bf5e0deba5eea7777447e6955d5441f8c37b
Michael <michaelbaltazar265@gmail.com>
2021-06-30 21:16:56 +0000

    adding mobile programming llc and practicing question

---
+++ just-practice/main.js
@@ -0,0 +1,20 @@
+
+
+console.log("hello Michael")
+
+
+function myFunction(num) {
+
+    console.log(num.toFixed(2))
+    // return num.toFixed(2)
+ }
+
+ myFunction(2.12397)
+ myFunction(1.12397)
+ myFunction(26.1379)
+ myFunction(3.136)
+
+//  2.12
+// main.js:8 1.12
+// main.js:8 26.14
+// main.js:8 3.14
