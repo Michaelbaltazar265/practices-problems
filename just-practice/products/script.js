1011d49e69953e57dc9aee9ad44857ddc161f025
Michael <michaelbaltazar265@gmail.com>
2021-07-10 00:01:52 +0000

    updating my practice folder

---
+++ just-practice/products/script.js
@@ -0,0 +1,24 @@
+
+
+
+// alert("Hello World!");
+
+
+// window.addEventListener('load', function() {
+//     let products = document.getElementById('products')
+//     console.log(products)
+//   })
+
+
+// const say = (word) =>{ 
+//     return word; 
+// }
+
+// console.log(say("Michael"))
+
+
+const add = (a, b) => { 
+    return a + b; 
+}
+
+console.log(add(5, 31))
